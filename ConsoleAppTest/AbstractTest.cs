7cd2c8a1cf2ae9904b1fc0580e9adb7cbce079fb
Paweł Łęczycki <pawel@msd.info.pl>
2024-02-20 14:07:55 +0100

    Abstract class

---
+++ ConsoleAppTest/AbstractTest.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+    public class AbstractTest
+    {
+
+
+        [Fact]
+        public void MyTest()
+        {
+            var myClass = new Mock<MyClass>();
+
+            var result = myClass.Object.DoSth();
+
+            result.Should().Be("");
+        }
+
+        public abstract class MyClass
+        {
+
+            public string DoSth()
+            {
+                return "";
+            }
+
+        }
+    }
+}
